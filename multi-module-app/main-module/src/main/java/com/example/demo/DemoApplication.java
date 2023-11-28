d07d76de1eda5ec9b6d4245164560b413b5f2a4f
Agnecious <AgneciousNkwana@gmail.com>
2023-11-28 16:42:14 +0200

    Update DemoApplication.java

--- multi-module-app/main-module/src/main/java/com/example/demo/DemoApplication.java
+++ multi-module-app/main-module/src/main/java/com/example/demo/DemoApplication.java
@@ -2,20 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-      //  @GetMapping("/name")
-	//public String getName() {
-	//	return "Mathema";
-	//}
+        @GetMapping("/name")
+	public String getName() {
+		return "Mathema";
+	}
 
 }
