4253e01b794d219d8a17205f32a6ef56b1cc6eb0
Agnecious <AgneciousNkwana@gmail.com>
2023-11-28 16:26:13 +0200

    Update DemoApplication.java

--- multi-module-app/main-module/src/main/java/com/example/demo/DemoApplication.java
+++ multi-module-app/main-module/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
